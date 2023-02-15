7269d0c33b451ffae690a27e2b3878503a1b0305
Hendrik López <hendriklopez2106@gmail.com>
2023-02-15 16:13:01 -0500

    se agregan navbar y sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {faUser, faBox, faSpa, faHotel} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'untitled2';
+  public faUser = faUser;
+  public faBox = faBox;
+  public faSpa = faSpa;
+  public faHotel = faHotel;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppComponent } from './app.component';
 import { ProductosComponent } from './productos/productos.component';
 import { ServiciosComponent } from './servicios/servicios.component';
 import { InmueblesComponent } from './inmuebles/inmuebles.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterOutlet} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
@@ -14,7 +23,16 @@ import { InmueblesComponent } from './inmuebles/inmuebles.component';
     InmueblesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterOutlet,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatSidenavModule,
+    FontAwesomeModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
