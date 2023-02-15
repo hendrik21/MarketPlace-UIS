d888765380c1141ec8d7559131dfe68292b79075
Hendrik López <hendriklopez2106@gmail.com>
2023-02-15 13:58:01 -0500

    se agregan componentes para productos, servicios e inmuebles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { InmueblesComponent } from './inmuebles/inmuebles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    ServiciosComponent,
+    InmueblesComponent
   ],
   imports: [
     BrowserModule
